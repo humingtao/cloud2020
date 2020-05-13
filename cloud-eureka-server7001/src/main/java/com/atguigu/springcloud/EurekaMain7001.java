540989ecb3bf6b0dcf02b4bed59b4fd92834f3a2
humingtao <983086529@qq.com>
2020-05-13 13:47:28 +0800

    构建Eureka    EurekaClient端cloud-provider-payment8001将注册进EurekaServer成为服务提供者provider

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,16 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author humingtao
- * @create 2020/5/11 21:18
+ * @create 2020/5/13 13:36
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+/*服务注册端*/
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
