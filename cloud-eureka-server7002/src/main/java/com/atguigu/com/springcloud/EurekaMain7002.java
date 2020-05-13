7ad582ce8e16e3a2a7f51b4071f46932358e0409
humingtao <983086529@qq.com>
2020-05-13 22:18:57 +0800

    构建Eureka 集群环境

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/com/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.atguigu.com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author humingtao
+ * @create 2020/5/13 22:15
+ */
+@SpringBootApplication
+/*服务注册端*/
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
